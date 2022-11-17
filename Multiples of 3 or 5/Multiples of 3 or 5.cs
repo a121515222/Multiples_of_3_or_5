465248a31e72cc9ea030f94809a29d7dfdf07f22
chunchia.liang <chunchia.liang61@titansoft.com.sg>
2022-11-17 12:16:29 +0800

    first commit

---
+++ Multiples of 3 or 5/Multiples of 3 or 5.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Multiples_of_3_or_5
+{
+    static class Multiples
+    {
+        public static int Multiples3Or5(int num)
+        {
+            int result = 0;
+            int sum = 0;
+            for (int i = 0; i < num ; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    result++;
+                    sum += i;
+                }
+                else if (i % 5 == 0 || i % 3 == 0)
+                {
+                    result++;
+                    sum += i;
+                }
+            }
+
+            if (sum > 0)
+            {
+                return sum;
+            }
+            else
+            {
+                return result;
+            }
+            
+        }
+    }
+}
---
+++ Multiples of 3 or 5/Run.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Multiples_of_3_or_5;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Multiples.Multiples3Or5(10));
+    }
+}
