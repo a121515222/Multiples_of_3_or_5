4736b144a17250bb0cf1fb6baeec49c4bf999211
chunchia.liang <chunchia.liang61@titansoft.com.sg>
2022-12-11 23:24:58 +0800

    Change unittest name

--- UniTestMultiples_of_3_or_5/UniTestMultiples of 3 or 5.cs
+++ UniTestMultiples_of_3_or_5/UniTestMultiples of 3 or 5.cs
@@ -1,22 +1,23 @@
 using Multiples_of_3_or_5;
 using NUnit.Framework;
-namespace UniTestMultiples_of_3_or_5;
+
+namespace TestProject1;
 
 public class UnitTest
 {
     
     [TestCase(4)]
-    public void TestDivisibleby3(int num)
+    public void Divisibleby3Test(int num)
     {
         Assert.AreEqual(3, Multiples.Multiples3Or5(num));
     }
     [TestCase(6)]
-    public void TestDivisibleby5(int num)
+    public void Divisibleby5Test(int num)
     {
         Assert.AreEqual(8, Multiples.Multiples3Or5(num));
     }
     [TestCase(16)]
-    public void TestDivisibleby3And5(int num)
+    public void Divisibleby3And5Test(int num)
     {
         Assert.AreEqual(60, Multiples.Multiples3Or5(num));
     }
